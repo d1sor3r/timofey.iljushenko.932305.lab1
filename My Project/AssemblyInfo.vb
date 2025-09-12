6e919e07d3f0c42a49abd7dd1f302ec3037986d4
User <User@DESKTOP-GMHG9KT>
2025-09-12 12:31:36 +0300

    Добавьте файлы проекта.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Управление общими сведениями о сборке осуществляется с помощью 
+' набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+' связанных с этой сборкой.
+
+' Просмотр значений атрибутов сборки
+<Assembly: AssemblyTitle("timofey.iljushenko._932305.lab1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("timofey.iljushenko._932305.lab1")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+<Assembly: Guid("4b6b93ed-3828-40e7-bca8-e60b55c3e87c")>
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
